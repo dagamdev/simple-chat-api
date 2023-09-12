89bc7f113ebc3590968ece9979bf5b1b72647bbe
Dagadev <gacub20ru@gmail.com>
2023-09-12 17:03:32 -0600

    update config

--- simple_chat/asgi.py
+++ simple_chat/asgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from chat import routing  # Importa el archivo de enrutamiento WebSocket de tu aplicación
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simple_chat.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AllowedHostsOriginValidator(
+    AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+  ),
+  # "websocket": URLRouter(routing.websocket_urlpatterns),
+})
