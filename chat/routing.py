b649d7f5967fff3872a6f3eacab1164c512b9887
Dagadev <gacub20ru@gmail.com>
2023-09-15 21:48:09 -0600

    add conversation route

--- chat/routing.py
+++ chat/routing.py
@@ -2,5 +2,6 @@ from django.urls import path, re_path
 from . import consumers
 
 websocket_urlpatterns = [
-  re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+  # re_path(r'ws/conversation/(?P<conversation_id>\w+)/$', consumers.ConversationConsumer.as_asgi()),
+  path('ws/conversation/<str:conversation_id>/', consumers.ConversationConsumer.as_asgi()),
 ]
