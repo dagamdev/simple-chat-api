b45f9d16f59a7650990ff0a1ed0a4b149721f1ad
Dagadev <gacub20ru@gmail.com>
2023-09-19 22:15:34 -0600

    remove user serializer in participant from conversation serializer

--- chat/serializer.py
+++ chat/serializer.py
@@ -1,13 +1,17 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import CustomUser, Conversation, Message
 
 class UserSerializer(ModelSerializer):
+  email = serializers.CharField(required=False)
+  username = serializers.CharField(required=False)
+
   class Meta:
     model = CustomUser
     exclude = ['password', 'last_login', 'is_superuser']
 
 class ConversationSerializer(ModelSerializer):
-  participants = UserSerializer(many=True, read_only=True)
+  # participants = UserSerializer(many=True, read_only=True)
 
   class Meta:
     model = Conversation
