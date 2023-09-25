7b3fc0cfcc00f1bdc8ec63c8cb52150f9aa564c9
Dagadev <gacub20ru@gmail.com>
2023-09-25 13:43:26 -0600

    update

--- simple_chat/settings.py
+++ simple_chat/settings.py
@@ -49,15 +49,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            'hosts': [EnvVariables.REDIS_URL]
-        },
-    },
-}
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -90,7 +81,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'simple_chat.wsgi.application'
-ASGI_APPLICATION = 'simple_chat.asgi.application'
 
 
 # Database
@@ -146,6 +136,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+ASGI_APPLICATION = 'simple_chat.asgi.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [EnvVariables.REDIS_URL]
+        },
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
