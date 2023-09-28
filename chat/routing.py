ac926df74037937685295b3e8873236ec18c9e83
Dagadev <gacub20ru@gmail.com>
2023-09-28 13:11:48 -0600

    change routing & asgi config

--- chat/routing.py
+++ chat/routing.py
@@ -1,5 +1,5 @@
-from django.urls import path, re_path
-from . import consumers
+from django.urls import path
+from . import consumers 
 
 websocket_urlpatterns = [
   # re_path(r'ws/conversation/(?P<conversation_id>\w+)/$', consumers.ConversationConsumer.as_asgi()),
--- simple_chat/asgi.py
+++ simple_chat/asgi.py
@@ -8,24 +8,27 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import django
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from chat.consumers import ConversationConsumer
 from django.urls import path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simple_chat.settings')
-default_asgi_application = get_asgi_application()
+# default_asgi_application = get_asgi_application()
+django.setup()
+application = get_default_application
 
-application = ProtocolTypeRouter({
-  'http': default_asgi_application,
-  # "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+# application = ProtocolTypeRouter({
+#   'http': default_asgi_application,
+#   # "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
   
-  'websocket': AllowedHostsOriginValidator(
-    AuthMiddlewareStack(URLRouter([
-      path('ws/conversation/<str:conversation_id>/', ConversationConsumer.as_asgi())
-    ]))
-  )
-  # "websocket": URLRouter(routing.websocket_urlpatterns),
-})
+#   'websocket': AllowedHostsOriginValidator(
+#     AuthMiddlewareStack(URLRouter([
+#       path('ws/conversation/<str:conversation_id>/', ConversationConsumer.as_asgi())
+#     ]))
+#   )
+#   # "websocket": URLRouter(routing.websocket_urlpatterns),
+# })
