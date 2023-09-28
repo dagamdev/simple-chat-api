3c9295af05c64f60220a86c5c93f27a96c16ba6c
Dagadev <gacub20ru@gmail.com>
2023-09-28 13:11:25 -0600

    add routing

---
+++ simple_chat/routing.py
@@ -0,0 +1,15 @@
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+import chat.routing
+
+application = ProtocolTypeRouter({
+  'http': get_asgi_application(),
+  # "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+  
+  'websocket': AllowedHostsOriginValidator(
+    AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+  )
+  # "websocket": URLRouter(routing.websocket_urlpatterns),
+})
