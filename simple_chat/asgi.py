37c7b0b78a6c54e40f32526408b418e6b064c53a
Dagadev <gacub20ru@gmail.com>
2023-09-28 12:40:26 -0600

    add consumers urls

--- simple_chat/asgi.py
+++ simple_chat/asgi.py
@@ -8,22 +8,21 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-# from channels.auth import AuthMiddlewareStack
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-# from channels.security.websocket import AllowedHostsOriginValidator
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-# from chat import routing
-
-default_asgi_application = get_asgi_application()
+from chat import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simple_chat.settings')
+default_asgi_application = get_asgi_application()
 
 application = ProtocolTypeRouter({
-  "http": default_asgi_application,
+  'http': default_asgi_application,
   # "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
   
-  # "websocket": AllowedHostsOriginValidator(
-  #   AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
-  # ),
+  'websocket': AllowedHostsOriginValidator(
+    AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+  )
   # "websocket": URLRouter(routing.websocket_urlpatterns),
 })
