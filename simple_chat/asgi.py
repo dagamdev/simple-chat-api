75ed017df54eb0ca4e9bb22c14fd1248b1779404
Dagadev <gacub20ru@gmail.com>
2023-09-28 13:28:30 -0600

    change asgi config

--- simple_chat/asgi.py
+++ simple_chat/asgi.py
@@ -8,22 +8,24 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-import django
 from channels.routing import get_default_application
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simple_chat.settings')
 # default_asgi_application = get_asgi_application()
-django.setup()
-application = get_default_application()
+# django.setup()
+# application = get_default_application()
 
-# application = ProtocolTypeRouter({
-#   'http': default_asgi_application,
-#   # "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+application = ProtocolTypeRouter({
+  'http': get_asgi_application(),
+  # "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
   
-#   'websocket': AllowedHostsOriginValidator(
-#     AuthMiddlewareStack(URLRouter([
-#       path('ws/conversation/<str:conversation_id>/', ConversationConsumer.as_asgi())
-#     ]))
-#   )
-#   # "websocket": URLRouter(routing.websocket_urlpatterns),
-# })
+  'websocket': AllowedHostsOriginValidator(
+    AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+  )
+  # "websocket": URLRouter(routing.websocket_urlpatterns),
+})
