fb785f6aba34f425255f78e18c1b637532d9a2a9
Dagadev <gacub20ru@gmail.com>
2023-09-28 15:12:02 -0600

    update asgi config

--- simple_chat/asgi.py
+++ simple_chat/asgi.py
@@ -1,31 +1,7 @@
-"""
-ASGI config for simple_chat project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
+import django
 from channels.routing import get_default_application
-from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simple_chat.settings')
-# default_asgi_application = get_asgi_application()
-# django.setup()
-# application = get_default_application()
-
-application = ProtocolTypeRouter({
-  'http': get_asgi_application(),
-  # "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
-  
-  'websocket': AllowedHostsOriginValidator(
-    AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
-  )
-  # "websocket": URLRouter(routing.websocket_urlpatterns),
-})
+django.setup()
+application = get_default_application()
